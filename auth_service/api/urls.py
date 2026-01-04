f685c64f4ef74d726952c19853e2c519d36f5465
SaurabhSB07 <barkesaurabh07@gmail.com>
2026-01-04 13:29:32 +0530

    Added Redis caching for users endpoint (Task 4)

--- auth_service/api/urls.py
+++ auth_service/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView, AdminOnlyView
+from .views import CachedUsersView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('profile/', ProfileView.as_view()),
     path('admin-only/', AdminOnlyView.as_view()),
+    path('users/', CachedUsersView.as_view()),
 ]
--- auth_service/api/views.py
+++ auth_service/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.core.cache import cache
+
 
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer
@@ -12,6 +14,11 @@ from .permissions import IsAdmin
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+        cache.delete("users_list")  # invalidate cache
+
 
 
 # Login (JWT)
@@ -36,3 +43,22 @@ class AdminOnlyView(APIView):
         return Response({
             "message": "Welcome Admin. You have access to this endpoint."
         })
+
+#-----------------------------------------------------------------
+
+# Cached Users List (Task 4)
+class CachedUsersView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        cache_key = "users_list"
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        cache.set(cache_key, serializer.data, timeout=300)  # 5 minutes
+
+        return Response(serializer.data)
